f773a1f4aa86dddba700c772dcc4330c232d4d2e
liampmoore <16942895+liampmoore@users.noreply.github.com>
2020-03-09 13:38:45 -0400

    Added some request types, installed shortid, added json middleware

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require('express');
-
+const shortid = require('shortid');
 const server = express();
 
+let hubs = [];
+
+server.use(express.json()); //middleware
+
+server.get('/', (req, res) => {
+    console.log(req)
+    res.status(200).json({
+        message: "Hello. Welcome to the server."
+    })
+})
+
+server.post('/api/hubs', (req, res) => {
+
+    const hubInfo = {...req.body, id: shortid.generate()};
+    hubs.push(hubInfo);
+
+    res.status(201).json(hubInfo)
+})
 
 const PORT = 5000;
 server.listen(PORT, () => {
 console.log(`\n ** API running on http://localhost:${PORT} **\n`);
-console.log(server);
+
 }
 
 );
